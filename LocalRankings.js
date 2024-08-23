ae0341cf16b2a7233352f90e52c7b9ed4c98c237
Xiang511 <toby91511@gmail.com>
2024-08-24 06:59:19 +0800

    feat: LocalRanking.js

---
+++ LocalRankings.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const path = require("path");
+
+dotenv.config({path:"./config.env"}); //讀取環境變數
+let api_key = process.env.API_KEY;
+let local = process.env.local;
+
+async function pathoflegend_getData() {
+    const url = `https://api.clashroyale.com/v1/locations/${local}/pathoflegend/players`;
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + api_key
+    };
+
+    try {
+        const response = await fetch(url, { headers });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        fs.writeFile('json/Taiwan.json', JSON.stringify(json), (err) => {
+            if (err) {
+            console.error(err);
+            } else {
+            
+            }
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+pathoflegend_getData();
+
+exports.pathoflegend_getData = pathoflegend_getData;
